7348cb7cba3f14510df77fdfce32eeb22ad37eb9
sinnnosukegg <shinnosukegg@icloud.com>
2021-02-01 15:59:29 +0900

    yu

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :create, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -14,9 +15,11 @@ class PrototypesController < ApplicationController
     end
   end
 
-
   def edit
     @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -52,9 +55,4 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:content, :image, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
+  #get 'prototypes/index'
   root to: "prototypes#index"
   resources :prototypes do
     resources :comments, only: :create
